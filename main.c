4f408521580820353796651eb8dfc1e421d12781
Reinaldo2619 <161092816+Reinaldo2619@users.noreply.github.com>
2024-04-19 19:06:00 +0000

    Menu

--- main.c
+++ main.c
@@ -2,3 +2,27 @@
 //     git add nome
 //     git commit -m "escreva"
 //     git push -u origin main
+
+#include <stdio.h>
+#include "contatos.h"
+
+int main() {
+    Agenda agenda;
+    inicializar(&agenda);
+
+  int opcao;
+  char nome[50], sobrenome[50], gmail[50], telefone[50];
+
+  do{
+    printf("\nMenu:\n");
+    printf("1- Adicionar contato\n");
+    printf("2- Lista de contatos\n");
+    printf("3- Excluir contato\n");
+    printf("4- Salvar agenda de contatos\n");
+    printf("5- Carregar agenda de contatos\n");
+    printf("0- Sair");
+    printf("Escolha uma opção: ");
+    scanf(%d, &opcao);
+
+    switch(opcao){
+  
