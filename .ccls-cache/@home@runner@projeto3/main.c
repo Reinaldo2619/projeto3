9d2d7f01395e58e5a2bec7b4b1fac7d340ef6f19
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-04-19 23:43:23 +0000

    modificando

--- .ccls-cache/@home@runner@projeto3/main.c
+++ .ccls-cache/@home@runner@projeto3/main.c
@@ -1,1 +1,13 @@
-//gcc *.c && ./a.out
+#include <stdio.h>
+#include "contatos.h"
+#include <string>
+
+int main(){
+  Contato contatos[MAX_CONTATOS];
+  int total_contatos = 0;
+  int opcao;
+  
+} 
+
+
+
