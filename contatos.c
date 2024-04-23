5997cfcffd6030c6902656a1a8a98cf32459921c
unifmausilva <161088037+unifmausilva@users.noreply.github.com>
2024-04-23 22:43:41 +0000

    resolvido bugs e implementado criar contato

--- contatos.c
+++ contatos.c
@@ -1,21 +1,37 @@
-#include <stdio.h>
 #include "contatos.h"
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void CadastrarContato(Contato agenda[], int *ncontato){
-  if(*ncontato >= MAX_CONTATOS){
-    printf("Não é possível cadastrar mais contatos, Excedeu o limite máximo de contatos\n");
+void CadastrarContato(Contato agenda[], int *ncontato) {
+  if (*ncontato >= MAX_CONTATOS) {
+    printf("Não é possível cadastrar mais contatos, Excedeu o limite máximo de "
+           "contatos\n");
     return;
   }
+
   printf("Digite o nome :");
-  scanf("%s", agenda[ncontato].nome);
+  if (scanf(" %s", agenda[*ncontato].nome) != 1) {
+    printf("erro ao ler o nome\n");
+  }
+  return;
+
   printf("Digite o sobrenome :");
-  scanf("%s", agenda[*ncontato].sobrenome);
+  if (scanf(" %s", agenda[*ncontato].sobrenome) != 1) {
+    printf("erro ao ler o sobrenome\n");
+  }
+  return;
+
   printf("Digite o email :");
-  scanf("%s", agenda[*ncontato].email);
+  if (scanf(" %s", agenda[*ncontato].email) != 1) {
+    printf("erro ao ler o email\n");
+  }
+  return;
+
   printf("Digite o telefone :");
-  scanf("%s", agenda[*ncontato].tel);
-  (*ncontato)++;
-  
-}
+  if (scanf(" %s", agenda[*ncontato].tel) != 1) {
+    printf("erro ao ler o telefone\n");
+    return;
+  }
+    (*ncontato)++;
+  }
