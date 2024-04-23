cba59bc3366ec838064f0c37730edac1e550d1c0
Reinaldo2619 <161092816+Reinaldo2619@users.noreply.github.com>
2024-04-23 22:47:34 +0000

    contatos.h corrigido, contatos.c ainda dando erro

--- contatos.h
+++ contatos.h
@@ -3,13 +3,13 @@
 //     git commit -m "escreva"
 //     git push -u origin main
 
-#indef CONTATOS_H
+#ifndef CONTATOS_H
 #define CONTATOS_H
 
 // Define uma constante MAX_CONTATOS para indicar o maximo de contatos que podem ser armazenados
 #define MAX_CONTATOS 255 
 
-// Define uma estrutura contato que armazena informaçoes sobre um unico contato (nome, sobrenome, gmail, telefone)
+// Define a estrutura contato que guarda informaçoes sobre um contato (nome, sobrenome, gmail, telefone)
 typedef struct {
     char nome[50];
     char sobrenome[50];
@@ -17,16 +17,18 @@ typedef struct {
     char telefone[50];
 } Contato;
 
-// Define uma estrutura agenda que contém um array de contatos
+// Define uma estrutura agenda que contém um array
 typedef struct {
     Contato contatos[MAX_CONTATOS];
     int quantidade;
 } Agenda;
 
-// Inicializar a agenda, adicionar um novo contato, listar contatos, excluir um contato, salvar a agenda em um arquivo e carregar a agenda de um arquivo estao definidas em contatos.c
+// Inicializar agenda, adicionar contato, lista de contatos, excluir contato, salvar agenda e carregar a agenda (todos estao definidos em contatos.c)
 void inicializarAgenda(Agenda *agenda);
 int adicionarContato(Agenda *agenda, const char *nome, const char *sobrenome, const char *email, const char *telefone);
 void listarContatos(const Agenda *agenda);
 int deletarContato(Agenda *agenda, const char *telefone);
 void salvarAgenda(const Agenda *agenda, const char *nomeArquivo);
 void carregarAgenda(Agenda *agenda, const char *nomeArquivo);
+
+#endif 
